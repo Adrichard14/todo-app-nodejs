d75802cccf10f5d696c26e1714fcc71dd5796213
adrichard14 <adrichard14@hotmail.com>
2022-12-17 21:44:31 -0300

    feat: adds docker

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,18 +1,17 @@
-import envSchema from "env-schema";
-import { Type, Static } from "@sinclair/typebox";
+import envSchema from 'env-schema';
+import { Type, Static } from '@sinclair/typebox';
 
 const schema = Type.Object({
   PORT: Type.Number({
-    default: 4000
+    default: 4000,
   }),
   HOST: Type.String({
-    default: "127.0.0.1"
+    default: '0.0.0.0',
   }),
   DATABASE_URL: Type.String(),
 });
 
-
-type Env = Static<typeof schema>
+type Env = Static<typeof schema>;
 
 export const config = envSchema<Env>({
   schema,
